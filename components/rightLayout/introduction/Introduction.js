a95ea3ef535ba6e172817840464a1e8f4c50ad18
Wilfer Salazar <wilfersalazar19@gmail.com>
2024-10-31 13:13:44 -0500

    Update Introduction.js

--- components/rightLayout/introduction/Introduction.js
+++ components/rightLayout/introduction/Introduction.js
@@ -1,11 +1,10 @@
-import cx from 'classnames';
-import styles from '../styles/introduction.module.scss';
+import styles from '../introduction.module.scss';
 
 const Introduction = ({ introductionInfo }) => {
     return (
         <div className="px-5 pt-4 pb-2">
             <h2>{introductionInfo.title}</h2>
-            <p className={cx('justify-content-center', styles['introduction__text'])}>
+            <p className={styles['introduction__text']}>
                 {introductionInfo.description?.replace('**', '\n')}
             </p>
         </div>
